554935e769580728c38b38ccb5f4d11b0b283b5a
Dorian <dorian@mino.rs>
2019-11-18 23:52:19 +0000

    MEG sequestered from behavioural testing

--- condition matrixes for testing/evaccum_condition_matrix.m
+++ condition matrixes for testing/evaccum_condition_matrix.m
@@ -6,6 +6,9 @@ clc;
 d.easy_rule = 80;
 d.hard_rule = 10;
 
+% how many blocks
+p.num_blocks = 20;
+
 % trial settings (*p.stim_mat* = parameter required to calculate stimulus condition matrix)
 p.num_trials_per_block = 64; % *p.stim_mat*
 p.num_cues = 4; % *p.stim_mat*
@@ -23,12 +26,13 @@ p.dot_motion_directions = union([p.cue_directions+d.easy_rule],[p.cue_directions
 %  7)  match blue (1) or match orange (2)
 %  8)  matching difficulty (1 = easy, 2 = difficult)
 %  9)  gives you a unique number for each trial condition
+% 10)  gives a number based on 9 to identify each response for each trial condition
 % note: if you try to test with two matching angles that are the same, you
 %       will get an error, so make them different by at least 1 degree. this is
 %       because we use 'union()' to calc matching distance from cue direction.
 
 % create matrix
-p.stim_mat = zeros(p.num_trials_per_block,9);
+p.stim_mat = zeros(p.num_trials_per_block,10);
 
 % create columns
 p.stim_mat(:,1) = sort(repmat(1:p.num_cues,[1,p.num_trials_per_block/p.num_cues]));
@@ -41,6 +45,7 @@ p.stim_mat(:,6) = min(dist,[],2);
 p.stim_mat(:,7) = (p.stim_mat(:,6)>90)+1;
 p.stim_mat(:,8) = ~((p.stim_mat(:,6)==min(p.stim_mat(:,6)))|(p.stim_mat(:,6)==max(p.stim_mat(:,6))))+1;
 p.stim_mat(:,9) = 1:length(p.stim_mat(:,9));
+p.stim_mat(:,10) = p.stim_mat(:,9)+p.num_trials_per_block;
 
 % clear floating variables
 clear dist;
