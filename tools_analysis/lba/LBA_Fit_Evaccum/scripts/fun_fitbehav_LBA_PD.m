99794e16dd8327707eec4a8d95bb7fbf3579473f
dorian-pc <dorian@mino.rs>
2020-03-05 17:37:14 +0000

    ready to run on scheduler

--- tools_analysis/lba/LBA_Fit_Evaccum/scripts/fun_fitbehav_LBA_PD.m
+++ tools_analysis/lba/LBA_Fit_Evaccum/scripts/fun_fitbehav_LBA_PD.m
@@ -1,4 +1,4 @@
-function fun_fitbehav_LBA_PD(settings,data)
+function fun_fitbehav_LBA_PD(settings)
 % Fit LBA model to RDK-tapping task
 % Use for  3-choice task: CONDITIONS segregated
 % JZ 15/10/2014 - 4-choice tapping task
@@ -11,7 +11,12 @@ rng(rseed,'twister') % for reproducibility
 %clear all
 %%
 savename = settings.savename;
+data = settings.data;
+randiter  = settings.randiter;
+nosession = settings.nosession;
 Model_Feature = settings.modfeat; % model variant for this job
+numParam   = getModelParam_cell_RDK(Model_Feature,2); % the 2 here reflects response options
+parange=[zeros(1,numParam);zeros(1,numParam)+10]';
 
 % optional but doesn't save time (thought it would be faster than normal
 % gradient descent, but isn't)
@@ -29,7 +34,7 @@ num_subjs = sum(~cellfun('isempty',{data.id})); % get the number of not empty ar
 %initialise cells
 data2fit={};
 
-for idxsubj = 1:length(num_subjs)
+for idxsubj = 1:num_subjs
     %%
     clear rt conds resp acc % clear vars from behavioural data to avoid reading mixed subject data
     fprintf('loading subject %1.0f \n',data(idxsubj).id);
@@ -73,61 +78,21 @@ for idxsubj = 1:length(num_subjs)
         data2fit{idxsubj,level}.minRT = round(minRT(idxsubj),2);
         %parange(end,1) = round(minRT(idxsubj),2);%constrain the lower bound of T0 to the shortest RT
     end
-    %%
-end
-
-
-
-% fit the basic model
-% startpar=[1,1,1,1,1, .1 .3]; % initial parameter [boundary,four drift rate, non-decisiontime, drift rate std]
-% upper staring-point is fixed
-%randiter = 500;%100; % randome search 100 iters before optimization
-%nosession = 100;%100;%20; % 20 optimization iterations
-
-randiter  = settings.randiter;
-nosession = settings.nosession;
-
-
-% Model fitting
-
-% model features that are available
-% 1- std differs across fingers
-% 2- C0  differs across fingers
-% 3- B differs across conditions
-% 4- Drift rate differs across conditions
-% 5- t0 differs across conditions
-
-%design_space={};
-% design_space={[1,3],[1,4],[1,3,4],[1,3,4,5],[1,2],[1,2,3],[1,2,4],[1,2,3,4],[1,2,3,4,5]};
-% Model_Feature=[1  4];
-%
-% numParam   =getModelParam_cell_RDK(Model_Feature,4);
-%
-% parange=[zeros(1,numParam);zeros(1,numParam)+10]';
-
-
-%%
-
-numParam   = getModelParam_cell_RDK(Model_Feature,2); % the 2 here reflects response options
-parange=[zeros(1,numParam);zeros(1,numParam)+10]';
-
-
-for idxsubj = 1:length(num_subjs)
-    
+    %% fit the model
     [bestpar{idxsubj,1},bestval{idxsubj,1},BIC{idxsubj,1}]=fitparams_refine_template_RDK('fiterror_cell_RDK',Model_Feature,{data2fit{idxsubj,1:4}},randiter,nosession,[],parange,bayesian);%#ok
     
 end
 
 save(savename,'bestpar','bestval','BIC','rseed','settings');%,'bestpar_PA','bestval_PA','BIC_PA')
 
+% this needs to come out of this script - just do it for the winning model
 % [~,id] = min(bestval);
-% params = bestpar(id,:);
+% bestparams = bestpar(id,:);
 % 
-% [~,parLL,parHL,parLH,parHH]=getModelParam_cell_RDK(mod_feature,2,param);
-% % you need to pull this stuff from fiterror_cell_RDK I think. Have made
-% % some modifications there but no idea how to get it to here
+% [~,parLL,parHL,parLH,parHH]=getModelParam_cell_RDK(mod_feature,2,bestparams);
 % params = struct();
-% params{1} = parLL;
+% params{1} = parLL; % not sure brace indexing is the go, might need normal
+% - we'll see
 % params{2} = parLH;
 % params{3} = parHL;
 % params{4} = parHH;
--- tools_analysis/lba/LBA_Fit_Evaccum/scripts/run_fitbehav_PD.m
+++ tools_analysis/lba/LBA_Fit_Evaccum/scripts/run_fitbehav_PD.m
@@ -7,8 +7,6 @@
 % to run go to matlab opened terminal and run:
 % squeue -u username (dm06)
 
-% do 100 optimisations
-
 %% set up
 
 close all;
@@ -25,26 +23,28 @@ t = struct(); % for temp vars
 rootdir = 'C:\Users\doria\Nextcloud\desiderata\desiderata\04 Research\05 Evidence Accumulation\01 EvAccum Code';%'\\cbsu\data\Group\Woolgar-Lab\projects\Dorian\EvAccum'; % root directory - used to inform directory mappings
 
 % only required if not testing
-datadir = fullfile(rootdir,'data\behav_pilot_2');
+datadir = fullfile(rootdir,'data','behav_pilot_2');
 lbadatadir = fullfile(datadir,'lba_fit'); % expects to find your data here and will save results in a sub-folder here
 p.data_name = 'prepped_data.mat'; % data file name
+jobdir = fullfile(lbadatadir,'scheduled_jobs'); % where you'll save any scheduled jobs (i.e. running on the scheduler)
+toolsdir = fullfile(rootdir, 'tools_analysis','lba','LBA_Fit_Evaccum','scripts'); % where are all your scripts/tools?
 
 p.save_name = 'Model_%s.mat';
 p.rng_seed = 17; % the rng seed number - fixed for reproducibility
 t.local = 1; % run locally? Or 0 will use cbu scheduler
-p.testing = 1; % if you want to use testing data, then switch to 1 and add the data folder to the path, else to 0. will save to pwd/test_results/
+p.testing = 0; % if you want to use testing data, then switch to 1 and add the data folder to the path, else to 0. will save to pwd/test_results/
 t.subject = 1; % if testing, which subject do you want to run?
 t.test_data_name = 'lba_test_data.mat'; % name of your test data
 
 % these are all the model variants we want to test - different combinations of free parameters
 p.design_space={[1,3],[1,4],[1,3,4],[1,3,4,5],[1,2],[1,2,3],[1,2,4],[1,2,3,4],[1,2,3,4,5],[1,5],[1,3,5],[1,4,5],[1,2,5],[1,2,3,5],[1,2,4,5]};
 settings.randiter  = 100; % random search iters before optimization
-settings.nosession = 1; % optimization iterations - more equals less chance of ending up in a local minimum
+settings.nosession = 100; % optimization iterations - more equals less chance of ending up in a local minimum
 settings.overwrite = 1;
 
 % directory mappings
 if ~p.testing
-    addpath(genpath(fullfile(rootdir, 'tools_analysis'))); % add tools folder to path (includes LBA scripts)
+    addpath(genpath(fullfile(toolsdir))); % add tools folder to path (includes LBA scripts)
     p.save_path = fullfile(lbadatadir, 'results');
     if ~exist(p.save_path,'dir')
         mkdir(p.save_path);
@@ -53,14 +53,14 @@ end
 
 % get the data
 if p.testing
-   warning('you are running in test mode; saving into current folder');
-   p.save_path = fullfile(pwd,'test_results');
-   if ~exist(p.save_path,'dir')
-       mkdir(p.save_path);
-   end
-   
-   t.alldata = load(t.test_data_name);
-   t.data = t.alldata.d.subjects(t.subject,:);
+    warning('you are running in test mode; saving into current folder');
+    p.save_path = fullfile(pwd,'test_results');
+    if ~exist(p.save_path,'dir')
+        mkdir(p.save_path);
+    end
+    
+    t.alldata = load(t.test_data_name);
+    t.data = t.alldata.d.subjects(t.subject,:);
 else
     t.fileinfo = dir(fullfile(lbadatadir,p.data_name));
     t.datapath = fullfile(lbadatadir,t.fileinfo.name);
@@ -75,9 +75,8 @@ data = t.data; % here's the data
 %% Prepare for parallel processing
 if ~p.testing && ~t.local
     fprintf('prepping for parallel processing %s\n', mfilename);
-    % OUT OF DATE 28 FEB 2020
+
     S = cbu_scheduler('custom',{'compute',30,5,36000});
-    jobdir = ''; error('fix this');
     if ~exist(jobdir,'dir')
         mkdir(jobdir);
     else
@@ -87,14 +86,16 @@ if ~p.testing && ~t.local
     S.JobStorageLocation = jobdir;
     
     
-    % up to date - tells the scheduler the directories needed for the analysis
-    % (so will need to duplicate your own genpaths)
-    error('be generous')
-    dependencies_path = {  cant genpath - must add each individually?      
+    % tells the scheduler the directories needed for the analysis (no
+    % genpath apparently, so need to specify subdirectories too
+    dependencies_path = {
+        lbadatadir
+        jobdir
+        toolsdir
+        fullfile(toolsdir,'Templates') % this gets included in local runs with genpath
+        p.save_path
         };
 end
-
-funanon = @fun_fitbehav_LBA_PD;
 
 %% create the jobs
 fprintf('creating jobs %s\n', mfilename);
@@ -103,36 +104,38 @@ J = [];
 ind = 0;
 
 for imod = 1:length(p.design_space) % for the number of combinations of free parameters you specified earlier (model variants)
-        p.numfile = num2str(imod);
-        t.subj_file_name = sprintf(p.save_name,p.numfile);
-        settings.savename = fullfile(p.save_path,t.subj_file_name);  
-        settings.modfeat  = p.design_space{imod}; % saves the model (which free parameters)
-        settings.rseed = p.rng_seed; % fixed for reproducibility
-        settings.data = data;
-
-        % then, depending on overwrite setting, add this job
-        if ~exist(settings.savename,'file')||settings.overwrite
-            ind = ind + 1;
-            J(ind).task = funanon; % run the fitting function as the task
-            J(ind).n_return_values = 0;
-            J(ind).input_args = {settings};
-            J(ind).depends_on = 0; % if this requires previous scripts to be run (i.e. 'ind' =1 needs be be done before 'ind' =2)
-        end
-
+    p.numfile = num2str(imod);
+    t.subj_file_name = sprintf(p.save_name,p.numfile);
+    settings.savename = fullfile(p.save_path,t.subj_file_name);
+    settings.modfeat  = p.design_space{imod}; % saves the model (which free parameters)
+    settings.rseed = p.rng_seed; % fixed for reproducibility
+    if p.testing
+        settings.data = data(t.subject);
+    else
+        settings.data = data; % note that this is fine for a small amount of data, but you will need to make it pull data files individually per job with bigger data sets or it will be very slow
+    end
+    
+    % then, depending on overwrite setting, add this job
+    if ~exist(settings.savename,'file')||settings.overwrite
+        ind = ind + 1;
+        J(ind).task = @fun_fitbehav_LBA_PD; % run the fitting function as the task
+        J(ind).n_return_values = 0;
+        J(ind).input_args = {settings};
+        J(ind).depends_on = 0; % if this requires previous scripts to be run (i.e. 'ind' =1 needs be be done before 'ind' =2)
+    end
+    
 end; clear imod ind;
 
 %% submit the jobs
 fprintf('submitting jobs from %s\n', mfilename);
 
-if p.testing % test on one subject
-    warning('you are testing locally on subject %1.0f',t.subject);
+if t.local || p.testing % run locally
+    warning('you are running locally\n');
+    if p.testing
+        warning('you are testing locally on subject %1.0f\n',t.subject);
+    end
     for ijob = 1:length(J)
-        fun_fitbehav_LBA_PD(J(ijob).input_args{1},data(t.subject))
-    end; clear ijob;
-elseif t.local && ~p.testing % run locally
-    warning('you are running locally, on all participants');
-    for ijob = 1:length(J)
-        fun_fitbehav_LBA_PD(J(ijob).input_args{1},data)
+        fun_fitbehav_LBA_PD(J(ijob).input_args{1}); % specification of {1} is to make sure the settings are entered in the right format
     end; clear ijob;
 else % submit the jobs to the cluster
     cbu_qsub(J, S, dependencies_path) % then submit
