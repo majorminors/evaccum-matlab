f78cd3bff92b00280dc59e350e8129961e54d8d5
dorian <dorian@mino.rs>
2020-09-16 19:47:36 +0100

    update for testing

--- tools_exp/MEG_trigger_test.m
+++ tools_exp/MEG_trigger_test.m
@@ -15,14 +15,14 @@ p = struct(); % est structure for parameter values
 % settings
 p.pre_reset_pause = 0.005; % in secs
 p.post_reset_pause = 1; % in secs
-p.trigger_values = [1,2,4,8]; % [1:1:255];
+p.trigger_values = [1:1:255]; %[1,2,4,8];
 
 % make sure pause is on
 pause('on');
 
 % enable MEG
 MEG = MEGSynchClass;
-pause(p.pausetime); % I don't know if this is necessary, but seems sensible to wait a sec for it to load the class
+pause(1); % I don't know if this is necessary, but seems sensible to wait a sec for it to load the class
 MEG.SendTrigger(0); % reset triggers
 
 % begin test
