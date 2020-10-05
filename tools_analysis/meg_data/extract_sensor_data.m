a7940f88b0c85d7b10c1f1b0d27519ac4598001c
dorian <dorian@mino.rs>
2020-10-05 19:49:29 +0100

    shape sensor data into cosmo format

--- tools_analysis/meg_data/extract_sensor_data.m
+++ tools_analysis/meg_data/extract_sensor_data.m
@@ -1,11 +1,23 @@
 % AW 5/8/20
+%% set up
+
+close all;
+clearvars;
+clc;
+
+fprintf('setting up %s\n', mfilename);
+p = struct(); % est structure for parameter values
+d = struct(); % est structure for data
+t = struct(); % another structure for temp vars
 
 % extract preprocessed sensor data into cosmo friendly format
+% set up some directory paths
 rootdir    = '/group/woolgar-lab/projects/Dorian/EvAccum/';
 megdatadir = fullfile(rootdir, 'data/meg_pilot_1/megdata/');
 behavdatadir   = fullfile(rootdir,'data/meg_pilot_1/behavioural/');
 behavfile = fullfile(behavdatadir,'S03/subj_3_MEGRTs.mat');
 
+% add tools
 addpath(genpath('/imaging/local/software/spm_cbu_svn/releases/spm12_fil_r7487')); %spm
 addpath('/group/woolgar-lab/projects/Dorian/EvAccum/tools_analysis/meg_data'); %where Ale's extract_chans function lives
 addpath(genpath('/group/woolgar-lab/projects/Tijl/MD_dtb/Data_and_Analysis/Toolboxes/CoSMoMVPA-master/')) %cosmo
@@ -14,9 +26,12 @@ addpath(genpath('/group/woolgar-lab/projects/Tijl/MD_dtb/Data_and_Analysis/Toolb
 
 %filename = '/imaging/at07/Matlab/Projects/Dorian/evaccum2/data/meg_pilot_1/megdata/subj_3/MEEG/Preprocess/SL_subj_3.mat';
 %filename='/imaging/aw02/tempEA/SL_subj_3.mat';
-filename='/group/woolgar-lab/projects/Dorian/EvAccum/data/meg_pilot_1/megdata/subj_3/MEEG/Preprocess/SL_subj_3.mat';
-[EEG,MEGMAG,MEGPLANAR,conditions,chanlabels,badchans, trialinfo] = extract_chans_withtrialnums(filename);
-    % re-composed matrix looks like:
+% extract the sensor data
+p.filename='/group/woolgar-lab/projects/Dorian/EvAccum/data/meg_pilot_1/megdata/subj_3/MEEG/Preprocess/SL_subj_3.mat';
+[EEG,MEGMAG,MEGPLANAR,conditions,chanlabels,badchans, trialinfo] = extract_chans_withtrialnums(p.filename);
+
+% now we'll pull the behaviour data
+    % a reminder that it looks like:
     %  1)  cue direction (1-4)
     %  2)  dot motion direction condition (1-8)
     %  3)  coherence difficulty (1 = easy, 2 = hard)
@@ -30,13 +45,51 @@ filename='/group/woolgar-lab/projects/Dorian/EvAccum/data/meg_pilot_1/megdata/su
     % 11)  something to tag which run this sequence of data belongs to
     % 12)  we later add a row of unique numbers to tag each trial
 load(behavfile,'MEG_RT'); % load the behavioural data
-condnums = double(MEG_RT(:,5)); % pull the condition numbers and convert them from string array to number array
+p.condnums = double(MEG_RT(:,5)); % pull the condition numbers and convert them from string array to number array
 % so now we have the condition numbers (condnums) and the trials are tagged
 % with the value needed to index into condnums (first row of trialinfo)
 
-% Yields channel * timepoints * trials matrices
+% now we have the sensor data from three sources (EEG + 2xMEG) in the
+% format: channel * timepoints * trials matrices
+% cosmo wants it as trials * (timepoints*channels)
 
-% cosmo likes it as trials and timepoints*channels (these are in columns, each column is a MEG channel and the timepoints stack columnwise)
+% if we want to play with all three sources, we need to stack the
+% channels so let's check they're sized properly
+if size(EEG,3) ~= size(MEGMAG,3) || size(EEG,3) ~= size(MEGPLANAR,3)
+    error('it appears your EEG and MEG data dont have the same number of trials');
+elseif size(EEG,2) ~= size(MEGMAG,2) || size(EEG,2) ~= size(MEGPLANAR,2)
+    error('it appears your EEG and MEG data dont have the same number of timepoints');
+end
+
+% now we'll convert them into the cosmo format
+t.num_trials = size(EEG,3); % we'll use EEG to size, but could be any source
+t.num_timepoints = size(EEG,2); % since they should be the same size
+% stack the three sets of channels for a reshape
+t.combined = []; % init this bad boy
+for itrial = 1:t.num_trials
+    t.combined(:,:,itrial) = [EEG(:,:,itrial);MEGMAG(:,:,itrial);MEGPLANAR(:,:,itrial)];
+end
+ds.samples = reshape(t.combined,t.num_trials,[]); % here we slide the trials (D3) into the rows, and we free the columns so matlab can stack D1 and D2 into them
+
+% % this one works but it takes bloody ages
+% t.cosmo = []; % init this bad boy
+% %t.cosmo is going to be trials * (timepoints*channels)
+% for itrial = 1:t.num_trials % for each trial
+%     t.alltrialdata = [];
+%     for itimepoint = 1:t.num_timepoints % for each timepoint
+%        % so now we want to move the row to a column
+%        t.eeg_only = [];
+%        t.eeg_megmag = [];
+%        t.all = [];
+%        t.eeg_only = EEG(:,itimepoint,itrial)';
+%        t.eeg_megmag = [t.eeg_only,MEGMAG(:,itimepoint,itrial)'];
+%        t.eeg_megmag_megplanar = [t.eeg_megmag,MEGPLANAR(:,itimepoint,itrial)'];
+%        t.alltrialdata = [t.alltrialdata,t.eeg_megmag_megplanar];
+%    end
+%    t.cosmo(itrial,:) = t.alltrialdata;
+% end
+
+
 % this is feature attributes (for each column what time and what channel)-ds.fa.chan % ds.fa.time, then ds.sa.chunks = chunks, and finally ds.a - attributes (copy them from the cosmo website) - a reminder of your stuff
 
 % next: reshape into cosmo format: 
