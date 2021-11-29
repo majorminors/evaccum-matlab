ab21b4a5d67588cb5ae5f54c5346ccd808bbf96c
dorian <dorian@mino.rs>
2021-11-29 15:19:29 +0000

    update import/organise scans with overwrite function and fix typos; add basic maxfilter script for debugging (definately some filename stuff that won't work)

--- tools_analysis/a1_importAndOrganiseScans.m
+++ tools_analysis/a1_importAndOrganiseScans.m
@@ -3,13 +3,14 @@ function a1_importAndOrganiseScans(thisSubject)
 % set data directory
 rootdir = '/imaging/woolgar/projects/Dorian/evaccum/evaccum-matlab';
 datadir = fullfile(rootdir,'data','meg_pilot_3');
+overwrite = 0; % turn this on for auto overwrite
 
 % using ale's language to avoid errors in changing the names
 base = fullfile(datadir,num2str(thisSubject.subj));
 
 % if it exists choose whether to overwrite
 fprintf('creating folder %s: \n',base);
-if exist(base,'dir')
+if exist(base,'dir') && ~overwrite
     t.prompt = 'this folder already exists, overwrite? ([y]/n) ';
     t.ok = input(t.prompt,'s');
     if isempty(t.ok); t.ok = 'y';
@@ -18,7 +19,7 @@ if exist(base,'dir')
     end
 end
 
-mkdir(base);
+if ~exist(base,'dir'); mkdir(base); end
 cd(base);
 
 %create my folders
@@ -32,7 +33,7 @@ source      = thisSubject.meg_folder;
 sourcefiles = cellfun(@(x) sprintf('%s.fif',x),thisSubject.meg_runs, 'UniformOutput',false);
 runlabs     = thisSubject.meg_labs;
 
-runfiles    = cellfun(@(x) sprintf([base,'MEEG/%s_raw.fif'],x), runlabs,'UniformOutput',false);
+runfiles    = cellfun(@(x) sprintf([base,'%s_raw.fif'],x), runlabs,'UniformOutput',false);
 
 cd(source);
 cellfun(@copyfile,sourcefiles,runfiles,'UniformOutput',false);
@@ -66,7 +67,7 @@ if ~isempty(thisSubject.mri)
     
 end
 
-cd([base,'MEEG']); save([thisSubject.subj,'_filesID.mat'],'runfiles','sourcefiles','thisSubject');
+cd(base); save([thisSubject.subj,'_filesID.mat'],'runfiles','sourcefiles','thisSubject');
 
 return
 end
---
+++ tools_analysis/a2_maxFilter.m
@@ -0,0 +1,51 @@
+function a2_maxFilter(thisSubject)
+
+
+overwrite = 0; % turn this on for auto overwrite
+
+addpath /hpc-software/matlab/cbu/
+addpath(genpath('/group/woolgar-lab/projects/Dorian/evaccum/evaccum-matlab/tools_analysis/'))
+droot = ['/imaging/woolgar/projects/Dorian/evaccum/evaccum-matlab/data/meg_pilot_3/' num2str(thisSubject.subj)];
+maxfld= fullfile(droot,MaxfilterOutput);
+addpath(droot);
+
+
+
+settings.overwrite = overwrite;
+
+% get the names of the .fif files in the subjects' data folder
+d = getnames(droot,[],'*raw.fif');
+
+settings.maxfld = maxfld;
+
+
+if~exist(fullfile(droot,['subjectInfo_',num2str(thisSubject.subj),'.mat']),'file')
+    save(fullfile(droot,['subjectInfo_',num2str(thisSubject.subj),'.mat']),'thisSubject');% save subject's details
+end
+
+if ~isempty(thisSubject.bad_meg) %either use operator's bad channesl or automatic bad channels detection
+    if numel(thisSubject.bad_meg)>9; thisSubject.bad_meg = thisSubject.bad_meg(1:9);end %maxfilter doesn't work with more than 10 bad chans
+    settings.badchans=['-autobad off -bad', sprintf(' %d',thisSubject.bad_meg)];
+else
+    settings.badchans = sprintf(' -autobad %d -badlimit %d',900,7);
+end
+
+
+for ifile = 1:length(d)
+    % what's happening here? what is it pulling from d? might need a
+    % fullfile operation
+    settings.infname{ifile} = [droot, d{ifile}];
+    %always match output's name to input's file to avoid mislabeling
+    settings.outfname{ifile}= sprintf([maxfld,'Run%s%s.fif'],dname{subi},char(regexp(d{ifile},'(\d*_)*','match')),dname{subi});
+end
+
+[PATHSTR,NAME,EXT] = fileparts(settings.outfname{ifile});
+
+if overwrite || ~exist([PATHSTR,'/',NAME,'_trans',EXT],'file')
+    fun_MaxFilter_RDKPD(settings)
+end
+
+
+
+return
+end
