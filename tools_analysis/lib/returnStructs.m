c933a8b0692b5b92e3e0a49ebce6f8a2ad7a203c
dorian <dorian@mino.rs>
2023-09-08 00:22:01 +0100

    add comments to returnstructs function

--- tools_analysis/lib/returnStructs.m
+++ tools_analysis/lib/returnStructs.m
@@ -1,4 +1,12 @@
+% structure is a cell array of participants
+% each cell has a bunch of fields that are structures---each of these is a fieldtrip
+% timelocked average
+% some subjects don't exist (i.e. empty cells)
+% so this gets all the non-empty cells, then pulls out the structure that
+% corresponds to the field for each of those
+
 function results = returnStructs(structure, field)
+
 
 getNotEmpty = @(x) find(~cellfun(@isempty,x));
 
