08d69401ef4558ca00443784886882812619bcc1
dorian <dorian@mino.rs>
2023-10-05 17:39:53 +0100

    update model params script to produce better tables for jasp

--- tools_analysis/import_model_parameters.m
+++ tools_analysis/import_model_parameters.m
@@ -2,8 +2,10 @@ clear all
 
 rootdir = '/imaging/woolgar/projects/Dorian/evaccum/evaccum-matlab';
 
-subs = readtable(fullfile(rootdir,'dmc_lba','subjectParams.csv'));
-mns = readtable(fullfile(rootdir,'dmc_lba','meanParams.csv'));
+dmcdir = fullfile(rootdir,'dmc_lba');
+
+subs = readtable(fullfile(dmcdir,'subjectParams.csv'));
+mns = readtable(fullfile(dmcdir,'meanParams.csv'));
 
 % plot([mns.b_ecer mns.b_echr mns.b_hcer mns.b_hchr],'linestyle','none','marker','o')
 % plot([mns.v1_ecer mns.v1_echr mns.v1_hcer mns.v1_hchr],'linestyle','none','marker','o')
@@ -13,14 +15,28 @@ mns = readtable(fullfile(rootdir,'dmc_lba','meanParams.csv'));
 
 % Load data from the subs table
 b_columns = subs(:, {'Var1' 'b_ecer', 'b_echr', 'b_hcer', 'b_hchr'});
+b_vals = stack(b_columns,b_columns.Properties.VariableNames(2:end));
+b_vals.Properties.VariableNames{1} = 'Subject';
+b_vals.Properties.VariableNames{2} = 'Param';
+b_vals.Properties.VariableNames{3} = 'Value';
 
-Subjects = b_columns.Var1;
-Values = table2array(b_columns(:, 2:end)); % If values start from column 2
-Conditions = b_columns.Properties.VariableNames(2:end); % Assuming b_columns is a table
-
-% If you need to repeat Subjects and Conditions according to the number of conditions
-Subjects_repeated = repelem(Subjects, numel(Conditions));
-Conditions_repeated = repmat(Conditions, numel(Subjects), 1);
-Values_flattened = reshape(Values.',[],1);
+writetable(b_columns,fullfile(dmcdir,'b_for_jasp.csv'))
 
 v1_columns = subs(:, {'Var1' 'v1_ecer', 'v1_echr', 'v1_hcer', 'v1_hchr'});
+v1_vals = stack(v1_columns,v1_columns.Properties.VariableNames(2:end));
+v1_vals.Properties.VariableNames{1} = 'Subject';
+v1_vals.Properties.VariableNames{2} = 'Param';
+v1_vals.Properties.VariableNames{3} = 'Value';
+
+writetable(v1_columns,fullfile(dmcdir,'v1_for_jasp.csv'))
+
+% so v1: f(3)=40.043 MS=8.262 p<0.001
+% all post-hoc t-tests significantly difference <0.001, except HcEr and
+% HcHr (0.412)!
+
+% in contrast, the b looks actually like they aren't different!
+
+
+writetable(join(b_columns,v1_columns,'Keys','Var1'),fullfile(dmcdir,'params_for_jasp.csv'))
+
+forPlot = vertcat(b_vals,v1_vals);
