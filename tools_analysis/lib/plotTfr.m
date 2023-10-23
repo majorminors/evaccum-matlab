0e2e89b64b6652a4b2a43ed320ce6ba8cccc076e
dorian <dorian@mino.rs>
2023-10-23 15:16:11 +0100

    use heatmap instead of imagesc, because colourbar was unmanageable

--- tools_analysis/lib/plotTfr.m
+++ tools_analysis/lib/plotTfr.m
@@ -4,33 +4,24 @@ figure('Position', [100 100 1600 1600]);
 cfg.figure = subplot(1,2,1);
 ft_singleplotTFR(cfg, ftDataStruct);
 title('TFR', 'FontSize', 12)
-pbaspect([1 1 1]);  % Makes this subplot square
+% pbaspect([1 1 1]);  % makes this subplot square
 
 subplot(1,2,2);
 load('bayes_colourmap.mat');
-exponential_minmax = 10;
-val_col_map = logspace(-exponential_minmax, exponential_minmax, size(colours, 1));
-imagesc(log10(bfs)); % plotting the tfr
-xticks(1:numel(ftDataStruct.time))
-xticklabels(ftDataStruct.time)
-xtickangle(45);
-yticks(1:numel(ftDataStruct.freq))
-yticklabels(floor(ftDataStruct.freq(end:-1:1)))
-% THIS MIGHT HELP WITH THE Z-AXIS!!
-% ax = gca;
-% set(ax,'YScale','log','XLim',[dataDiff.time(1),dataDiff.time(end)], ...
-%     'YLim',[1e-6 1e6],'YTick',10.^(-6:2:6))
-pbaspect([1 1 1]);  % Makes this subplot square
-% f = gcf; f.Position = [10 10 1600 1600];
-colormap(subplot(1,2,2),colours);
-% adjusting the tick labels for the colourbar
-cbh = colorbar;
-cbh.Ticks = [-exponential_minmax, -6, -3, -1, -0.5, 0, 0.5, 1, 3, 6, exponential_minmax];
-cbh.TickLabels = arrayfun(@(x) ['10^{' num2str(x) '}'], cbh.Ticks, 'UniformOutput', false);
-cbh.TickLabels(strcmp(cbh.TickLabels, '10^{0}')) = {'Inconclusive'};
-cbh.TickLabels(strcmp(cbh.TickLabels, '10^{0.5}') | strcmp(cbh.TickLabels, '10^{-0.5}')) = {'Moderate'};
-cbh.TickLabels(strcmp(cbh.TickLabels, '10^{1}') | strcmp(cbh.TickLabels, '10^{-1}')) = {'Strong'};
-title('Bayes Factors', 'FontSize', 12)
+h=heatmap(ftDataStruct.time,floor(ftDataStruct.freq(end:-1:1)),bfs,'ColorScaling','log');
+caxis([-5 5]) % this does not map properly to the colour values but [-5 5] gets +/-10^2
+h.Colormap=colours;
+h.GridVisible = 'off';
+xlabel('time (ms)');
+ylabel('frequency');
+% % temporarily change axis units 
+% originalUnits = h.Units;  % save original units (probaly normalized)
+% h.Units = 'centimeters';  % any unit that will result in squares
+% % make axes square
+% h.Position(3:4) = min(h.Position(3:4))*[1,1]; 
+% h.Units = originalUnits; 
+
+% title('Bayes Factors', 'FontSize', 12)
 
 if exist('savename', 'var') && ~isempty(savename)
     print(savename, '-dpng');
