61b5675c747ba168e5bf88868fe95bbcf5917d1f
dorian <dorian@mino.rs>
2023-10-27 11:38:03 +0100

    minor updates

--- Pipeline.m
+++ Pipeline.m
@@ -16,7 +16,7 @@ datadir = fullfile(rootdir,'data','meg_pilot_4'); addpath(datadir);
 runLocal = 0;
 runBehav = 0;
 subjectRange = 0;%[34 35 36 37];%0;%[-1 7]; % 0 does all; array like [-1 4] does 4 to end
-jobdir = fullfile(rootdir,'job_logging','rsa_08_inclconds');
+jobdir = fullfile(rootdir,'job_logging','rsa_10_secondattempt');
 % functionToRun = -@a1_importAndOrganiseScans; additionalParams={datadir,0};
 % functionToRun = @a2_megTriggers; additionalParams={datadir,scriptdir,0}; 
 % functionToRun = @a3_maxFilter; additionalParams={datadir,scriptdir,1};
@@ -25,7 +25,7 @@ jobdir = fullfile(rootdir,'job_logging','rsa_08_inclconds');
 % functionToRun = @a4_preProc_atypical_artefacts; additionalParams={datadir,scriptdir,1,{'if' '.fif' 'C'},0,0}; % run locally if manual
 % b1_data_inspection is a standalone file for local exploration
 % functionToRun = @b2_aggregate_data; additionalParams={datadir,scriptdir,0};
-functionToRun = @b2_run_rsa_analysis; additionalParams={datadir,scriptdir,0};
+functionToRun = @b2_run_rsa_analysis; additionalParams={datadir,scriptdir,1};
 
 allSubjects = importParticipants();
 if ~subjectRange; subjectRange = 1:numel(allSubjects); end
@@ -38,7 +38,7 @@ J(1:numel(allSubjects))=deal(struct('task',[],'n_return_values',[], ...
     'input_args',[],'depends_on',[]));
 
 for subjectidx = subjectRange
-    
+        
     thisSubject = allSubjects{subjectidx}; % let's make this easier to reference
     
     fprintf('%s (idx: %.0f)\n', thisSubject.id, subjectidx);
@@ -72,7 +72,7 @@ if ~runLocal
     % first because we prepopulated J
     clearArray = [];
     for i = 1:numel(J)
-        if isempty(J(i).task)
+        if isempty(J(i).task)   
             clearArray = [clearArray,i];
         end
     end
@@ -113,3 +113,5 @@ if ~runLocal
     % submit the jobs
     cbu_qsub(J,S,{rootdir,scriptdir},jobopts);
 end
+
+disp('done!')
