9ee88d02de664a386bd5c7bfdbf396718b963397
dorian <dorian@mino.rs>
2023-11-24 14:10:12 +0000

    make minor changes

--- Pipeline.m
+++ Pipeline.m
@@ -17,14 +17,14 @@ runLocal = 1;
 runBehav = 0;
 subjectRange = 0;%[34 35 36 37];%0;%[-1 7]; % 0 does all; array like [-1 4] does 4 to end
 jobdir = fullfile(rootdir,'job_logging','high_hanning');
-% functionToRun = -@a1_importAndOrganiseScans; additionalParams={datadir,0};
+% functionToRun = @a1_importAndOrganiseScans; additionalParams={datadir,0};
 % functionToRun = @a2_megTriggers; additionalParams={datadir,scriptdir,0}; 
 % functionToRun = @a3_maxFilter; additionalParams={datadir,scriptdir,1};
 % functionToRun = @a4_preProc_filtering; additionalParams={datadir,scriptdir,1,1};
 % functionToRun = @a4_preProc_ica; additionalParams={datadir,scriptdir,0,{'f' '.fif' 'i'},0}; % run locally if manual
 % functionToRun = @a4_preProc_atypical_artefacts; additionalParams={datadir,scriptdir,1,{'if' '.fif' 'C'},0,0}; % run locally if manual
 % b1_data_inspection is a standalone file for local exploration
-functionToRun = @b2_aggregate_data; additionalParams={datadir,scriptdir,1};
+% functionToRun = @b2_aggregate_data; additionalParams={datadir,scriptdir,1};
 % functionToRun = @b2_run_rsa_analysis; additionalParams={datadir,scriptdir,1};
 
 allSubjects = importParticipants();
